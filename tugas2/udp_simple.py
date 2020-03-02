ed2fbf5e0b266751c197589d514bc931f471fbad
Pristi Zahara <pristizzz@gmail.com>
2020-03-03 01:18:09 +0700

    tugas 2 dah

--- tugas2/udp_simple.py
+++ tugas2/udp_simple.py
@@ -1,7 +1,7 @@
 import socket
 
-TARGET_IP = "10.151.62.1"
-TARGET_PORT = 5005
+TARGET_IP = "127.0.0.1"
+TARGET_PORT = 5006
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(bytes('ABCDEFGHIJKLMNOPQRSTUV'.encode()),(TARGET_IP,TARGET_PORT))
--- tugas2/udpfileclient.py
+++ tugas2/udpfileclient.py
@@ -2,7 +2,7 @@ import socket
 import os
 
 TARGET_IP = "127.0.0.1"
-TARGET_PORT = 5005
+TARGET_PORT = 5006
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
